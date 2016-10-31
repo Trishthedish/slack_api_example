7e3e30073122b3c5114ba635527ae9293b0883f3
Trish Griego <pmgriego@gmail.com>
2016-10-31 13:30:30 -0700

    Following example

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ gem 'foundation-rails'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  # so we can keep track of secrets!
+
   gem 'dotenv-rails'
   gem 'better_errors'
 end
--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -1,7 +1,29 @@
 require 'httparty'
+require 'awesome_print'
 
 class Slack_Api_Wrapper
+  # you need to use a token
+  BASE_URL = "https://slack.com/api/"
+  TOKEN = ENV["SLACK_TOKEN"]
+  # The BASE_URL is the beginning of every request, and the TOKEN will be the Slack API token we made available through the .env file earlier.
 
+  def self.list_channels()
+    url = BASE_URL + "channels.list?token=#{TOKEN}"
+
+    response = HTTParty.get(url)
+
+    my_channels = []
+
+    response["channels"].each do |channel|
+      id = channel["id"]
+      name = channel["name"]
+      my_channels << Slack_Channel.new(name, id)
+    end
+    my_channels
+  end
+
+    def sendmsg(channel, message)
+    end
 
 
 end
